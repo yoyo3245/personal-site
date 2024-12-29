0f53e92a0ab65cbf049385db93ac18a8503032b9
yoyo3245 <157998034+yoyo3245@users.noreply.github.com>
2024-12-29 14:42:52 -0800

    landing page final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { useState } from 'react';
+
+var ReactRotatingText = require('react-rotating-text');
 
 function App() {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsAnimating(true);
+  };
+
+  const handleAnimationEnd = () => {
+    setIsAnimating(false);
+  };
+
   return (
     <div className="App">
       <Navbar />
       <header className="Home">
-        <p>
-          Yoseph Personal Site!!!
+        <h1>
+          Hey, I'm Yoseph!
+        </h1>
+        <h2 className='rotating-text'>
+          <ReactRotatingText items={['Software Engineer 💻', 'Student 🧑🏾‍🎓', 'Logic Lover 🤓', 'Batman 🦇']}/>
+        </h2>
+
+        <p className='description-text'>
+          An engineer passionate about building impactful projects and <br/>
+          exploring the intersection of human behavior and technology.
         </p>
+
+        <div className="arrow-container">
+          <i 
+            className={`bi bi-arrow-down ${isAnimating ? 'animating' : ''}`}
+            onMouseEnter={handleMouseEnter}
+            onAnimationEnd={handleAnimationEnd}
+          ></i>
+        </div>
+        
       </header>
     </div>
   );
