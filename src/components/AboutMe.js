98198f68e43d3ff3297df0b5fecad53e835eb7ba
yosephendawoke <ye38@cornell.edu>
2025-05-22 10:40:17 -0400

    added alt

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,13 +9,13 @@ export default function AboutMe() {
 
       <div className='flex justify-center items-center'>
         <div className='picture-container'>
-          <img className='picture' src={picture} />
+          <img className='picture' src={picture} alt="Portrait of Yoseph Endawoke" />
         </div>
 
         <div className='text'>
           <h1 className='header'>I'm a Software Engineer from Boston, Massachusetts.</h1>
           <p>
-            I'm a sophomore at <span style={{color: 'red'}}>Cornell University</span> studying <span style={{color: '#7AF980'}}>Computer Science</span> with a minor in <span style={{color: '#7AF980'}}>Applied Mathmatics</span>, and I'm passionate about building impactful projects that help people. 
+            I'm a sophomore at <span style={{color: 'red'}}>Cornell University</span> studying <span style={{color: '#7AF980'}}>Computer Science</span> with a minor in <span style={{color: '#7AF980'}}>Applied Mathematics</span>, and I'm passionate about building impactful projects that help people. 
             <br></br>
             <br></br>
             Outside of coding, I enjoy going to the gym and playing video games with my friends. 
